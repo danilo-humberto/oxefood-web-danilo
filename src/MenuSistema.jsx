96029d7eda4c65be6268733a496f055ad1384dc6
aluno <aluno@ifpe.edu.br>
2024-08-15 16:18:41 -0300

    colocando proteção nas rotas

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -1,56 +1,67 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import { logout } from './views/util/AuthenticationService';
 
-export default function MenuSistema(props){
+export default function MenuSistema(props) {
 
-    return(
-        <>
-            <Menu inverted>
-                
-                <Menu.Item
-                    name='home'
-                    active={props.tela === 'home'}
-                    as={Link}
-                    to='/'
-                />
+    const handleLogout = () => {
+        logout()
+    }
+    
+  return (
+    <>
+      <Menu inverted>
+        <Menu.Item
+          name="home"
+          active={props.tela === "home"}
+          as={Link}
+          to="/"
+        />
 
-                <Menu.Item
-                    name='cliente'
-                    active={props.tela === 'cliente'}
-                    as={Link}
-                    to='/form-cliente'
-                />
+        <Menu.Item
+          name="cliente"
+          active={props.tela === "cliente"}
+          as={Link}
+          to="/form-cliente"
+        />
 
-                <Menu.Item
-                    name='produto'
-                    active={props.tela === 'produto'}
-                    as={Link}
-                    to='/form-produto'
-                />
+        <Menu.Item
+          name="produto"
+          active={props.tela === "produto"}
+          as={Link}
+          to="/form-produto"
+        />
 
-                <Menu.Item
-                    name='entregador'
-                    active={props.tela === 'entregador'}
-                    as={Link}
-                    to='/form-entregador'
-                />
+        <Menu.Item
+          name="entregador"
+          active={props.tela === "entregador"}
+          as={Link}
+          to="/form-entregador"
+        />
 
-                <Menu.Item 
-                    name="fornecedor"
-                    active={props.tela === 'fornecedor'}
-                    as={Link}
-                    to='/form-fornecedor'
-                />
+        <Menu.Item
+          name="fornecedor"
+          active={props.tela === "fornecedor"}
+          as={Link}
+          to="/form-fornecedor"
+        />
 
-                <Menu.Item 
-                    name="fabricante"
-                    active={props.tela === 'fabricante'}
-                    as={Link}
-                    to='/form-fabricante'
-                />
+        <Menu.Item
+          name="fabricante"
+          active={props.tela === "fabricante"}
+          as={Link}
+          to="/form-fabricante"
+        />
 
-            </Menu>
-        </>
-    )
+        <Menu.Item
+          className="navbar__item--mobile"
+          onClick={handleLogout}
+          content="Sair"
+          as={Link}
+          to="/"
+        />
+      </Menu>
+    </>
+  );
 }
--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,32 +2,123 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import FormCliente from './views/client/FormCliente';
-import FormEntregador from './views/entregador/FormEntregador';
-import Home from './views/home/Home';
-import FormProduct from './views/product/FormProduct';
 import ListCliente from './views/client/ListCliente';
-import ListProduto from './views/product/ListProduto';
+import FormEntregador from './views/entregador/FormEntregador';
 import ListEntregador from './views/entregador/ListEntregador';
-import FormFornecedor from './views/fornecedor/FormFornecedor';
-import ListFornecedor from './views/fornecedor/ListFornecedor';
 import FormFabricante from './views/fabricante/FormFabricante';
 import ListFabricante from './views/fabricante/ListFabricante';
+import FormFornecedor from './views/fornecedor/FormFornecedor';
+import ListFornecedor from './views/fornecedor/ListFornecedor';
+import Home from './views/home/Home';
+import FormLogin from './views/login/FormLogin';
+import FormProduct from './views/product/FormProduct';
+import ListProduto from './views/product/ListProduto';
+import { ProtectedRoute } from './views/util/ProtectedRoute';
 
 function Rotas() {
     return (
         <>
             <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="list-cliente" element={ <ListCliente/> } />
-                <Route path="list-produto" element={ <ListProduto/> } />
-                <Route path="list-entregador" element={ <ListEntregador/> } />
-                <Route path="form-cliente" element={ <FormCliente/> } />
-                <Route path="form-produto" element={ <FormProduct/> } />
-                <Route path="form-entregador" element={ <FormEntregador/> } />
-                <Route path="form-fornecedor" element={ <FormFornecedor/> } />
-                <Route path="list-fornecedor" element={ <ListFornecedor /> }/>
-                <Route path='form-fabricante' element={<FormFabricante />} />
-                <Route path='list-fabricante' element={<ListFabricante />} />
+                <Route path="/" element={ <FormLogin /> } />
+
+                <Route 
+                    path='/home'
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/list-cliente'
+                    element={
+                        <ProtectedRoute>
+                            <ListCliente />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/list-produto'
+                    element={
+                        <ProtectedRoute>
+                            <ListProduto />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/list-entregador'
+                    element={
+                        <ProtectedRoute>
+                            <ListEntregador />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/list-fabricante'
+                    element={
+                        <ProtectedRoute>
+                            <ListFabricante />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/list-fornecedor'
+                    element={
+                        <ProtectedRoute>
+                            <ListFornecedor />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/form-cliente'
+                    element={
+                        <ProtectedRoute>
+                            <FormCliente />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/form-produto'
+                    element={
+                        <ProtectedRoute>
+                            <FormProduct />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/form-entregador'
+                    element={
+                        <ProtectedRoute>
+                            <FormEntregador />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/form-fornecedor'
+                    element={
+                        <ProtectedRoute>
+                            <FormFornecedor />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route 
+                    path='/form-fabricante'
+                    element={
+                        <ProtectedRoute>
+                            <FormFabricante />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </>
     )
